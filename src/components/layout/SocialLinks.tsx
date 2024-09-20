8be89cd711b6859af016f730e0d60f41ada66abb
sejarparvez <sejarparvez@yahoo.com>
2024-09-20 23:42:58 +0600

    fixed the social link

--- src/components/layout/SocialLinks.tsx
+++ src/components/layout/SocialLinks.tsx
@@ -1,46 +1,32 @@
 import Link from "next/link";
 import { BsFacebook } from "react-icons/bs";
-import { FaInstagram } from "react-icons/fa";
-import { FaGithub, FaLinkedinIn, FaXTwitter } from "react-icons/fa6";
+import { FaLinkedinIn, FaYoutube } from "react-icons/fa6";
 import ToolTipHook from "../helper/ToolTipHook";
 
 export default function SocialLinks() {
   return (
     <section className="flex flex-col gap-8 [&>*]:text-gray-300 hover:[&>*]:text-primary">
       <Link
-        href="https://facebook.com/sejarparvez"
+        href="https://facebook.com/people/Mibagu/61565820790664/"
         target="_blank"
         rel="noopener"
       >
         <ToolTipHook text="Facebook" icon={<BsFacebook size={28} />} />
       </Link>
+
       <Link
-        href="https://twitter.com/sejarparvez"
-        target="_blank"
-        rel="noopener"
-      >
-        <ToolTipHook text="Twitter" icon={<FaXTwitter size={28} />} />
-      </Link>
-      <Link
-        href="https://instagram.com/sejarparvez"
-        target="_blank"
-        rel="noopener"
-      >
-        <ToolTipHook text="Instagram" icon={<FaInstagram size={28} />} />
-      </Link>
-      <Link
-        href="https://linkedin.com/in/sejarparvez"
+        href="https://www.linkedin.com/in/trip2africa-travel-agency-719b35189/"
         target="_blank"
         rel="noopener"
       >
         <ToolTipHook text="Linkedin" icon={<FaLinkedinIn size={28} />} />
       </Link>
       <Link
-        href="https://github.com/sejarparvez"
+        href="https://www.youtube.com/@mibagu0"
         target="_blank"
         rel="noopener"
       >
-        <ToolTipHook text="GitHub" icon={<FaGithub size={28} />} />
+        <ToolTipHook text="YouTube" icon={<FaYoutube size={28} />} />
       </Link>
     </section>
   );
